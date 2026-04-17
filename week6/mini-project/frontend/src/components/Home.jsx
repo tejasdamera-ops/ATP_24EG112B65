854eb693014ff44ebbcfe87c4ff899e36053aee0
tejasdamera-ops <tejasdamera@gmail.com>
2026-04-17 11:42:05 +0530

    update

--- week6/mini-project/frontend/src/components/Home.jsx
+++ week6/mini-project/frontend/src/components/Home.jsx
@@ -1,36 +1,33 @@
-import {useContext} from 'react'
-import { counterContextObj } from '../context/ContextProvider'
-import Test from './Test';
-import { useCounterStore } from '../store/CounterStore';
+import { useContext } from "react";
+import { counterContextObj } from "../context/ContextProvider";
+import { useCounterStore } from "../store/CounterStore";
 
 function Home() {
   console.log("Home");
 
-
   // CALL useCounterStore hook to get state of ZUSTAND STORE
 
-  // let {newCounter,incrementCounter} =useCounterStore(); 
+  // let {newCounter,incrementCounter} =useCounterStore();
 
   // now i have more then one state so use counter store lo specific state ne use cheskovali
-  let newCounter = useCounterStore(state=>state.newCounter)
-  let incrementCounter=useCounterStore(state=>state.incrementCounter)
-  let newCounterSet=useCounterStore(state=>state.newCounterSet)
+  let newCounter = useCounterStore((state) => state.newCounter);
+  let incrementCounter = useCounterStore((state) => state.incrementCounter);
+  let newCounterSet = useCounterStore((state) => state.newCounterSet);
 
-  let user =useCounterStore(state=>state.user);
-  let changeNameAndAge=useCounterStore(state=>state.changeNameAndAge)
+  let user = useCounterStore((state) => state.user);
+  let changeNameAndAge = useCounterStore((state) => state.changeNameAndAge);
 
-
-
-  const {counter,changeCounter}=useContext(counterContextObj);
+  const { counter, changeCounter } = useContext(counterContextObj);
 
   return (
     <div>
       <p>Counter sharing across components::</p>
       <h1>Counter :{newCounter}</h1>
-      <button onClick={newCounterSet} className='bg-black text-white p-2 m-1'>Increment</button>
-      <Test/>
+      <button onClick={newCounterSet} className="bg-black text-white p-2 m-1">
+        Increment
+      </button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
